e7bf01e11c3c5a43db58d0732411d7ac11bf3ca7
Nichita Ursu <nichita.ursu@dassolutions.eu>
2020-05-24 16:54:26 +0300

    Initial commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Todo } from './todo-list/todo-list.component';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent {
+
+  todos: Todo[] = [
+    { title: 'Create App', description: 'Use Angular' },
+    { title: 'Buy Milk', description: 'In Linella' },
+    { title: 'Talk to John', description: 'John\'s number: 079xxxxxx' },
+  ];
+
+  handleTodoSubmited({ title, description }: Todo): void {
+    const isTodoAlreadyExists = this.todos.some((todo: Todo) => {
+      return (title === todo.title) && (description === todo.description);
+    });
+
+    if (!isTodoAlreadyExists) {
+      this.todos.push({ title, description });
+    }
+  }
+}
